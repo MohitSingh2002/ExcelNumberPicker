8bb637a599ab2921a7a063bd988d09b10c0987e6
Mohit <mohitsingh11082002@gmail.com>
2020-11-03 13:25:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/excelnumberpicker/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.excelnumberpicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.NumberPicker;
+
+import com.example.excelnumberpicker.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    boolean showColumnNumber = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setupButton();
+
+    }
+
+    private void setupButton() {
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.numberPicker.setVisibility(View.VISIBLE);
+                setupNumberPicker(binding.etNumber1.getText().toString().trim(), binding.etNumber2.getText().toString().trim());
+            }
+        });
+    }
+
+    private void setupNumberPicker(String min, String max) {
+        binding.numberPicker.setMinValue(Integer.parseInt(min));
+        binding.numberPicker.setMaxValue(Integer.parseInt(max));
+
+        binding.numberPicker.setFormatter(new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                return showColumnNumber ? value + " - " + getColName(value) : getColName(value);
+            }
+        });
+    }
+
+    public void changeColumnCondition(View view) {
+        showColumnNumber = !showColumnNumber;
+    }
+
+
+     private String getColName(int col) {
+         StringBuilder builder = new StringBuilder();
+         while (col > 0) {
+             int remainder = col % 26;
+             if (remainder == 0) {
+                 builder.append('Z');
+                 col = (col / 26) - 1;
+             } else {
+                 builder.append((char) (64 + remainder));
+                 col = col / 26;
+             }
+         }
+
+         return builder.reverse().toString();
+    }
+
+}
